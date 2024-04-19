91c8f6a39f031236ba6e202dd00c1c6617fa8ed0
hjopel <70332133+hjopel@users.noreply.github.com>
2024-04-19 07:21:14 +0200

    feat: draggable card

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ import {
   useSphericalJoint,
 } from '@react-three/rapier'
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
+import { Environment, Lightformer } from '@react-three/drei'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
 
@@ -23,7 +24,8 @@ const Band = () => {
     fixed = useRef<RapierRigidBody>(),
     j1 = useRef<RapierRigidBody>(),
     j2 = useRef<RapierRigidBody>(),
-    j3 = useRef<RapierRigidBody>()
+    j3 = useRef<RapierRigidBody>(),
+    card = useRef<RapierRigidBody>()
 
   const { width, height } = useThree((state) => state.size)
 
@@ -32,30 +34,67 @@ const Band = () => {
       new THREE.CatmullRomCurve3([new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()]),
   )
 
+  const vec = new THREE.Vector3()
+  const ang = new THREE.Vector3()
+  const rot = new THREE.Vector3()
+  const dir = new THREE.Vector3()
+  const [dragged, drag] = useState<THREE.Vector3 | false>()
+
   useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1])
   useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1])
   useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1])
+  useSphericalJoint(j3, card, [
+    [0, 0, 0],
+    [0, 1.45, 0],
+  ])
 
-  useFrame(() => {
-    curve.points[0].copy(j3.current.translation())
-    curve.points[1].copy(j2.current.translation())
-    curve.points[2].copy(j1.current.translation())
-    curve.points[3].copy(fixed.current.translation())
-    band.current.geometry.setPoints(curve.getPoints(32))
+  useFrame((state) => {
+    if (dragged) {
+      vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera)
+      dir.copy(vec).sub(state.camera.position).normalize()
+      vec.add(dir.multiplyScalar(state.camera.position.length()))
+      card.current.setNextKinematicTranslation({ x: vec.x - dragged.x, y: vec.y - dragged.y, z: vec.z - dragged.z })
+    }
+
+    if (fixed.current) {
+      curve.points[0].copy(j3.current.translation())
+      curve.points[1].copy(j2.current.translation())
+      curve.points[2].copy(j1.current.translation())
+      curve.points[3].copy(fixed.current.translation())
+      band.current.geometry.setPoints(curve.getPoints(32))
+
+      // Tilt the card back towards the screen
+      ang.copy(card.current.angvel())
+      rot.copy(card.current.rotation())
+      card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.25, z: ang.z })
+    }
   })
 
   return (
     <>
-      <RigidBody ref={fixed} type='fixed' />
-      <RigidBody position={[0.5, 0, 0]} ref={j1}>
-        <BallCollider args={[0.1]} />
-      </RigidBody>
-      <RigidBody position={[1, 0, 0]} ref={j2}>
-        <BallCollider args={[0.1]} />
-      </RigidBody>
-      <RigidBody position={[1.5, 0, 0]} ref={j3}>
-        <BallCollider args={[0.1]} />
-      </RigidBody>
+      <group position={[0, 4, 0]}>
+        <RigidBody ref={fixed} type='fixed' />
+        <RigidBody position={[0.5, 0, 0]} ref={j1}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody position={[1, 0, 0]} ref={j2}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody position={[1.5, 0, 0]} ref={j3}>
+          <BallCollider args={[0.1]} />
+        </RigidBody>
+        <RigidBody ref={card} type={dragged ? 'kinematicPosition' : 'dynamic'}>
+          <CuboidCollider args={[0.8, 1.125, 0.01]} />
+          <mesh
+            onPointerUp={(e) => drag(false)}
+            onPointerDown={(e) => drag(new THREE.Vector3().copy(e.point).sub(vec.copy(card.current.translation())))}
+          >
+            <planeGeometry args={[0.8 * 2, 1.125 * 2]} />
+            <meshBasicMaterial color='white' side={THREE.DoubleSide} />
+          </mesh>
+        </RigidBody>
+      </group>
+
       <mesh ref={band}>
         <meshLineGeometry />
         <meshLineMaterial color='white' resolution={[width, height]} lineWidth={1} />
@@ -66,22 +105,45 @@ const Band = () => {
 export default function Page() {
   return (
     <>
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
-        {/* jumbo */}
-        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
-        </div>
+      <Canvas camera={{ position: [0, 0, 13], fov: 25 }}>
+        <ambientLight intensity={Math.PI} />
 
-        <div className='w-full text-center md:w-3/5'>
-          <Canvas>
-            <Physics>
-              <Band />
-            </Physics>
-          </Canvas>
-        </div>
-      </div>
+        <Physics interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
+          <Band />
+        </Physics>
+
+        <Environment background blur={0.75}>
+          <color attach='background' args={['black']} />
+          <Lightformer
+            intensity={2}
+            color='white'
+            position={[0, -1, 5]}
+            rotation={[0, 0, Math.PI / 3]}
+            scale={[100, 0.1, 1]}
+          />
+          <Lightformer
+            intensity={3}
+            color='white'
+            position={[-1, -1, 1]}
+            rotation={[0, 0, Math.PI / 3]}
+            scale={[100, 0.1, 1]}
+          />
+          <Lightformer
+            intensity={3}
+            color='white'
+            position={[1, 1, 1]}
+            rotation={[0, 0, Math.PI / 3]}
+            scale={[100, 0.1, 1]}
+          />
+          <Lightformer
+            intensity={10}
+            color='white'
+            position={[-10, 0, 14]}
+            rotation={[0, Math.PI / 2, Math.PI / 3]}
+            scale={[100, 10, 1]}
+          />
+        </Environment>
+      </Canvas>
     </>
   )
 }
